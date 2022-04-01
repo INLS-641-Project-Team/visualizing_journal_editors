d2da632a007987bf0aef0b2a9ad02b61541b37cd
Rolando R <50704607+RolRodr@users.noreply.github.com>
2022-04-01 11:30:51 -0400

    updating website

--- docs/index.js
+++ docs/index.js
@@ -15,15 +15,10 @@ window.addEventListener("scroll", e => {
     }
 })
 
-
-let data = [...Array(11).keys()]
-
-
-
 class bannerVis {
-    constructor(div_id, data) {
+    constructor(div_id) {
         this.div_id = div_id;
-        this.data = data;
+        this.data = [...Array(11).keys()];
     }
 
     render() {
@@ -36,7 +31,7 @@ class bannerVis {
         // let scaleX = d3.scaleLinear().domain([0, 30]).range([-90 * (Math.PI / 180), 90 * (Math.PI / 180)]);
         // let scaleY = d3.scaleLinear().domain([0, 30]).range([380, 400]);
 
-        let icon_selection = imgDiv.selectAll(".book-icon").data(data);
+        let icon_selection = imgDiv.selectAll(".book-icon").data(this.data);
         let edge_selection = imgDiv.selectAll(".edge").data(edgeData);
 
         icon_selection.join(
@@ -79,7 +74,7 @@ class bannerVis {
     }
 }
 
-let bookVis = new bannerVis("introBanner", data);
+let bookVis = new bannerVis("introBanner");
 bookVis.render();
 
 class mapVis {
@@ -88,23 +83,24 @@ class mapVis {
         this.div_id = div_id;
     }
 
+
+
     async render() {
-
-        // load data
-        let map_data = await d3.json("world_geo.json", d => d)
-
 
         // select container and create visualization
         let svg = d3.select("#" + this.div_id)
         let svg_height = svg._parents[0].clientHeight
         let svg_width = svg._parents[0].clientWidth
-        let projection = d3.geoRobinson().scale(140).translate([svg_width / 2, svg_height / 3])
+        let projection = d3.geoRobinson().scale(130).translate([svg_width / 2, svg_height / 3])
         let path = d3.geoPath().projection(projection);
 
-        // prepare data
-        let data = await d3.csv("cleaned_csv2.csv", d => d);
+        // load and prepare data
+        let map_data = await d3.json("world_geo.json", d => d);
+        console.log(map_data);
+        let ed_data = await d3.csv("cleaned_csv2.csv", d => d);
+        console.log(ed_data);
         let country_conc = {}
-        data.forEach(d => !(d.country in country_conc) ? country_conc[d.country] = 1 : country_conc[d.country] += 1)
+        ed_data.forEach(d => !(d.country in country_conc) ? country_conc[d.country] = 1 : country_conc[d.country] += 1)
 
         let map_countries = []
         map_data.features.forEach(d => {
@@ -113,40 +109,49 @@ class mapVis {
         let data_countries = Array.from(Object.keys(country_conc));
         data_countries.map(d => d.trim().toLowerCase())
 
-        console.log(data_countries)
-        console.log(map_countries)
-
         map_data.features.forEach(d => {
-            if (data_countries.map(d => d.trim().toLowerCase()).includes(d.properties.geounit.trim().toLowerCase())) {
-                d.properties['editor_count'] = country_conc[d.properties.geounit.trim()]
-                console.log(`${d.properties.geounit} has ${d.properties['editor_count']} editors!`)
+            if (country_conc[d.properties.geounit]) {
+                d.properties['editor_count'] = country_conc[d.properties.geounit]
             } else if (d.properties.geounit == 'United States of America') {
-                d.properties['editor_count'] = country_conc['United States']
-                console.log(`${d.properties.geounit} has ${d.properties['editor_count']} editors!`)
+                d.properties['editor_count'] = country_conc["United States"]
             } else if (d.properties.geounit == 'Hong Kong S.A.R.') {
-                d.properties['editor_count'] = country_conc['Hong Kong']
-                console.log(`${d.properties.geounit} has ${d.properties['editor_count']} editors!`)
+                d.properties['editor_count'] = country_conc["Hong Kong"]
+            } else if (d.properties.geounit == 'Ivory Coast') {
+                d.properties['editor_count'] = country_conc["Côte d'Ivoire"]
+            } else if (d.properties.geounit == 'Syria') {
+                d.properties['editor_count'] = country_conc["Syrian Arab Republic"]
+            } else if (d.properties.geounit == 'South Korea') {
+                d.properties['editor_count'] = country_conc["Korea"]
+            } else if (d.properties.geounit == 'Guinea Bissau') {
+                d.properties['editor_count'] = country_conc["Guinea-Bissau"]
+            } else if (d.properties.geounit == 'The Bahamas') {
+                d.properties['editor_count'] = country_conc["Bahamas"]
+            } else if (d.properties.geounit == 'Macao S.A.R') {
+                d.properties['editor_count'] = country_conc["Macau"]
+            } else if (d.properties.geounit == 'Republic of Serbia') {
+                d.properties['editor_count'] = country_conc["Serbia"]
             } else {
-                console.log(`----${d.properties.geounit} didn't work!`)
+                d.properties['editor_count'] = 'Unknown'
             }
         })
 
-        data_countries.forEach(d => {
-            if (!map_countries.includes(d)) {
-                console.log(d)
-            }
-        })
 
         // create colorscale for map
-        let colorMap = d3.scaleLinear().domain([0, d3.max(Object.values(country_conc))]).range(["green", "#467aaa"]);
+        let colorMap = d3.scaleLinear().domain([0, 123844]).range(["#f2f2f2", '#467aaa']);
 
         // draw countries
         svg.append("g")
-            .attr("class", "country")
             .selectAll("path")
             .data(map_data.features)
             .join("path")
-            .attr("fill", d => colorMap(d.properties.editor_count))
+            .attr("class", "country")
+            .attr("id", d => `${d.properties.geounit}_drawing`)
+            .attr("fill", d => colorMap(d.properties.editor_count ? d.properties.editor_count : 0))
+            .attr("data-tippy-content", d => {
+                let html = `Country: ${d.properties.geounit} \n Editor Count: ${d.properties.editor_count}`
+                return html
+            })
+            .call(selection => tippy(selection.nodes(), { allowHTML: true, followCursor: true }))
             .attr("d", path)
 
         // draw lat/long lines
